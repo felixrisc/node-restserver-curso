e1486628dd8da4bedc6cf19af8e7bb3d13aa29f9
Félix <felixrisc@gmail.com>
2021-01-21 00:18:37 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 //             Puerto
 // =========================================
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================================
+//             Entorno
+// =========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================================
+//             Entorno
+// =========================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://lennon:vb0zSPW2uE8Uy3Gq@cluster0.jae45.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
